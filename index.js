1c8a5e6803ffccc0b81239186fbe3ba7637d6de0
Saksham Arora <isakshamarora@gmail.com>
2024-05-06 22:05:12 +0530

    Update application code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // index.js
-const config = require('./config.json');
+const env = process.env.NODE_ENV || 'dev';
+const config = require(`./config.${env}.json`);
 
-console.log('Environment:', process.env.NODE_ENV);
+console.log('Environment:', env);
 console.log('Config:', config);
+// index.js
 
